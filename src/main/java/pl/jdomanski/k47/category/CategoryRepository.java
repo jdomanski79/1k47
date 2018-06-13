b60d7367a51673137ca42f14483ab21076f85693
jd <jd@jd.com>
2018-06-13 11:08:12 +0000

    fix bug - Long -> Integer in CategoryRepository declaration

--- src/main/java/pl/jdomanski/k47/category/CategoryRepository.java
+++ src/main/java/pl/jdomanski/k47/category/CategoryRepository.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface CategoryRepository extends CrudRepository<Category, Long> {
+public interface CategoryRepository extends CrudRepository<Category, Integer> {
     
     public Category findByName(String name);
     public List<Category> findByType(CategoryType type);
