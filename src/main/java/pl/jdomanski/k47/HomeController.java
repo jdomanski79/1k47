f85af55bec4087a1a4df387ecfbc4eed64654cbd
jd <jd@jd.com>
2018-06-19 06:50:17 +0000

    Transaction.java - update DateTimeFormat pattern to HTML5 form date standard

--- src/main/java/pl/jdomanski/k47/HomeController.java
+++ src/main/java/pl/jdomanski/k47/HomeController.java
@@ -1,15 +1,35 @@
 package pl.jdomanski.k47;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import pl.jdomanski.k47.category.CategoryType;
+import pl.jdomanski.k47.transaction.Transaction;
+import pl.jdomanski.k47.transaction.TransactionRepository;
+
 @Controller
 @RequestMapping("/")
 public class HomeController {
+    @Autowired
+    TransactionRepository rep;
     
     @GetMapping
     public String home(){
+        
+        List<Object[]> list = rep.findByIncomeGroupByCategory(CategoryType.INCOME);
+        System.out.println("Found:");
+        for (Object[] o : list){
+            for (int i= 0; i < o.length; i++){
+                System.out.println(o[i]);
+            }
+            
+            
+        }
+        System.out.println("-----\n");
         return "home";
     }
 }
