ea24f680617439b769fae8a522f7017916a2a240
jd <jd@jd.com>
2018-07-06 12:48:40 +0000

    add Mappings class

---
+++ src/main/java/pl/jdomanski/k47/util/Mappings.java
@@ -0,0 +1,13 @@
+package pl.jdomanski.k47.util;
+
+public final class Mappings {
+    
+    // == CONSTANTS ==
+    // -- categories mappings --
+    private static final String CATEGORY_ROOT = "categories";
+    public static final String CATEGORIES = CATEGORY_ROOT;
+    public static final String CATEGORY_ADD = CATEGORY_ROOT + "/form";
+        
+    // == constructor ==
+    private Mappings(){}
+}
