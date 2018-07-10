1b07a250b950056fe60779e7d3df19c27d740e27
EC2 Default User <ec2-user@ip-172-31-22-152.us-east-2.compute.internal>
2018-07-10 09:12:11 +0000

    refactor to use constants

--- src/main/java/pl/jdomanski/k47/category/CategoryController.java
+++ src/main/java/pl/jdomanski/k47/category/CategoryController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -75,7 +76,7 @@ public class CategoryController {
 	
 		
 	@GetMapping(Mappings.CATEGORY_UPDATE)
-	public String categoryUpdateGet(@PathVariable int id, Model model){
+	public String categoryUpdateGet(@RequestParam int id, Model model){
 	    
 	    Category category = categoryService.findById(id);
 	    
